2692247f9de34bf38da500dbffd6f5049297141c
Shraddha Sandhansive <shraddha@Shraddhas-MacBook-Air.local>
2019-10-05 18:53:28 +0530

    Email settings.

--- api_example/settings.py
+++ api_example/settings.py
@@ -120,3 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# EMAIL_HOST_USER = "ankushgochke@gmail.com"
+# EMAIL_HOST_PASSWORD = "fcpark22"
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = "ankushgochke@gmail.com"
+EMAIL_HOST_PASSWORD = "shraddhagochke"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
